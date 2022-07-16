43db94aed994af195a032a53f522db463df13cff
sara tareq <53131646+saraatq@users.noreply.github.com>
2022-07-16 16:39:56 +0200

    fix filter tool

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = MultiApp()
 app.add_app("Image Segmentation Tool", segmentation.app)
 app.add_app("Reduce Periodic Noise Tool", noise.app)
 app.add_app("Histogram Equalization Tool", equalization.app)
-app.add_app("Filter Tool", apply_filter.app)
+app.add_app("laplacian Filter Tool", apply_filter.app)
 app.add_app("Show Image Histogram", histogram.app)
 app.add_app("Brightness Adjustment Tool", brightness.app)
 
